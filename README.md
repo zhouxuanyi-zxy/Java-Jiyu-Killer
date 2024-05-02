2cfdf8a167d9625cc2c0ea0675e241480a6b3f5a
zhouxuanyi_zxy <69704410+zhouxuanyi-zxy@users.noreply.github.com>
2024-05-02 23:00:23 +0800

    v1.01

---
+++ README.md
@@ -0,0 +1,49 @@
+## Java-Jiyu-Killer
+
+### 1.介绍
+
+一个我无聊时写的[Python-Nanruan-Killer](https://github.com/zhouxuanyi-zxy/Python-Nanruan-Killer)的java版
+
+其仍然使用pskill和ntsd及taskkill杀极域进程
+
++ 支持最小化极域屏幕广播
+
++ 支持缺少文件自动下载
+
+##### 至于为什么用Java?
+
+因为好用,而且好打包成.exe文件
+
+### 2.各版本的功能差异
+
+| 版本/特性                                                    | 杀死极域进程 | 检测所需文件是否存在 | 自动下载所需文件 | 自释放所需文件(base64) | 最小化屏幕广播   | 通过删除文件使极域停止运行 | <--的基础--自动从注册表读取极域程序地址 |
+| ------------------------------------------------------------ | ------------ | -------------------- | ---------------- | ---------------------- | ---------------- | -------------------------- | --------------------------------------- |
+| [Python-Nanruan-Killer](https://github.com/zhouxuanyi-zxy/Python-Nanruan-Killer) | ✔            | ✔                    | ✔                | ✔                      | ✔ (大部分情况下) | ✔                          | ✔                                       |
+| [Cpp-Nanruan-Killer](https://github.com/zhouxuanyi-zxy/Cpp-Nanruan-Killer) | ✔            | ×                    | ×                | ×                      | ×                | ×                          | ×                                       |
+| [CSharp-Jiyu-Killer](https://github.com/zhouxuanyi-zxy/CSharp-Jiyu-Killer) | ✔            | ✔                    | ×                | ×                      | ×                | ×                          | ×                                       |
+| [Java-Jiyu-Killer](https://github.com/zhouxuanyi-zxy/Java-Jiyu-Killer) | ✔            | ✔                    | ✔                | TODO                   | ✔ (未完全完成 )  | TODO                       | TODO                                    |
+
+** ✔ 为已经支持
+
+** × 为不支持(甚至以后可能也不会)
+
+** TODO 为有计划
+
+所以 Jiyu-Killer (Nanruan-Killer) 系列都是以python版本为基准的
+
+其它版本的功能差异还请谅解
+
+
+
+### 3.版本更新历史
+
+#### v1.01 (2024.5.2)
+
+1.自动检测所需文件
+
+2.自动下载所需文件
+
+3.支持最小化极域屏幕广播窗口(不过还未完工)
+
+4.杀极域进程
+
---
+++ src/Main.java
@@ -0,0 +1,121 @@
+/*
+    Jiyu Killer Java Edition
+    Version: v1.01
+    License: MIT License
+*/
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void fileExists(){
+        Path NowDir = Paths.get("").toAbsolutePath();
+        String NowPath = NowDir + "\\";
+        File ck_pskill = new File(NowPath + "pskill.exe");
+        File ck_ntsd = new File(NowPath + "ntsd.exe");
+        if (ck_pskill.exists() && ck_ntsd.exists()){
+            System.out.println("Files are OK");
+        }
+        if (!ck_pskill.exists()){
+            System.out.println("Missed pskill");
+            File file = new File(NowPath + "pskill.exe");
+            downloadFile("https://gitee.com/zhouxuanyi/Python-Nanruan-Killer/raw/master/pskill.exe", file);
+
+        }
+        if (!ck_ntsd.exists()){
+            System.out.println("Missed ntsd");
+            File file = new File(NowPath + "ntsd.exe");
+            downloadFile("https://gitee.com/zhouxuanyi/Python-Nanruan-Killer/raw/master/Python_Nanruan_Killer/ntsd.exe", file);
+        }
+        System.out.println("Files Now Are OK");
+    }
+    public static void downloadFile(String downloadUrl, File file) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            URL url = new URL(downloadUrl);
+            URLConnection connection = url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            int length = 0;
+            byte[] bytes = new byte[1024];
+            while ((length = inputStream.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0, length);
+            }
+            fileOutputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+            System.out.println("Download Error");
+        }
+    }
+    public static void StartProcess(String ps_name) {
+        List<String> ps = new ArrayList<>();
+        Path NowDir = Paths.get("").toAbsolutePath();
+        String ProgramPath;
+        ProgramPath = NowDir + "\\";
+        System.out.println(ProgramPath);
+        if (ps_name == "pskill"){
+            ProgramPath += ps_name + ".exe";
+            ps.add(ProgramPath);
+            ps.add("-t");
+            ps.add("-nobanner");
+            ps.add("StudentMain.exe");
+        }else if (ps_name == "ntsd"){
+            ProgramPath += ps_name + ".exe";
+            ps.add(ProgramPath);
+            ps.add("-c");
+            ps.add("q");
+            ps.add("-pn");
+            ps.add("StudentMain.exe");
+        }else if (ps_name == "taskkill") {
+            ps.add("C:\\Windows\\System32\\" + ps_name);
+            ps.add("/F");
+            ps.add("/T");
+            ps.add("/IM");
+            ps.add("StudentMain.exe");
+        }
+        ProcessBuilder psBuilder = new ProcessBuilder();
+        psBuilder.command(ps);
+        psBuilder.redirectErrorStream(true);
+        try {
+            Process ps_start = psBuilder.start();
+            ps_start.waitFor();
+            InputStream input = ps_start.getInputStream();
+            InputStream errors = ps_start.getErrorStream();
+            int data = 0;
+            String data1 = "";
+            while ((data = input.read()) != -1){
+                data1 += (char) data;
+            }
+//            System.out.println(data1);
+
+            data = 0;
+            data1 = "";
+            while ((data = errors.read()) != -1){
+                data1 += (char) data;
+            }
+//            System.out.println(data1);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void minimizeJiyu(String windowTitle){
+        WinDef.HWND hwnd = User32.INSTANCE.FindWindow(null,windowTitle);
+        User32.INSTANCE.ShowWindow(hwnd,User32.SW_MINIMIZE);
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println("# Jiyu Killer Java Edition");
+        System.out.println("# By Afefegeb");
+        Path NowDir = Paths.get("").toAbsolutePath();
+        String NowPath = NowDir + "\\";
+        fileExists();
+        StartProcess("taskkill");
+        StartProcess("pskill");
+    }
+}
